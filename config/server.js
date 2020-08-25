0dff212640863aa3c6962877788b8ea56489323f
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-08-25 12:14:29 -0700

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'afaf6cb271710321ec6d79613439cb9c'),
+    },
+  },
+});
